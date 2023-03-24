551a746180419d4dfcc26fd6b93b7383f16bb34d
juane382 <juane.palombo@gmail.com>
2023-03-24 15:41:44 -0300

    Prueba socket funcionando

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const ServerSocket = io('http://localhost:8080/')
 
+const btnPresioname = document.querySelector('#btnPresioname')
 
+if(btnPresioname){
+    btnPresioname.addEventListener(
+        'click',
+        e => {
+            ServerSocket.emit('mensajeDelCliente',  {datos: [1,2,3]})
+        }
+    )
+
+}
 
 ServerSocket.on('mesajito', datosAdjuntos => {
     console.log(datosAdjuntos)
--- src/servidor.js
+++ src/servidor.js
@@ -1,7 +1,15 @@
 import express from 'express'
-import {Server as SocketIOServer} from 'socket.io'
+import { engine } from 'express-handlebars'
+import { Server as SocketIOServer } from 'socket.io'
+import { productManager } from './productsManager.js'
+
+const productos = new productManager('./src/localStorage/productos.json')
 
 const app = express()
+
+app.engine('handlebars', engine())
+app.set('views', './views')
+app.set('view engine', 'handlebars')
 
 app.use(express.static('./src'))
 
@@ -9,9 +17,20 @@ const httpServer = app.listen(8080)
 
 const io = new SocketIOServer(httpServer)
 
-io.on('connection',clienteSocket =>{
+io.on('connection', clienteSocket => {
     console.log(`nuevo cliete conectado. Socket id:  ${clienteSocket.id}`)
-    clienteSocket.emit('mesajito',{hola:'HolaMundo'})
+    clienteSocket.emit('mesajito', { hola: 'HolaMundo' })
     clienteSocket.emit('alerta', 'hola que onda!')
+    clienteSocket.on('mensajeDelCliente',datosAdjuntos => {
+        console.log(`${clienteSocket.id} dice:`)
+        console.log(datosAdjuntos)
+    })
 })
 
+app.get('/', async (req, res) => {
+    const mensajes = await productos.getProducts()
+    res.render('realTimeProducts', {
+        hayMensajes: mensajes.lenght > 0,
+        mensajes
+    })
+})
